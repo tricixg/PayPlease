531d7e011f2d5cac9482025751d0ee08e65ce0fa
tricixg <64681919+tricixg@users.noreply.github.com>
2023-09-03 16:11:44 +0800

    custom topup amount v1
    1)custom amount is entered via input
    2)user clicks the button to initiate payment

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({ extended: false }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/user", userRouter);
 
